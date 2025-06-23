6989016d3cae029c7fcad1aa8dabec6944c8ba38
yoshixi <davidchenyuhe@proton.me>
2025-06-23 14:18:08 -0400

    fix : pulling from origin and readding ellipse files

---
+++ Ellipse.java/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Ellipse.java {
+}
---
+++ lab5/src/lab5/EllipseTest.java
@@ -0,0 +1,14 @@
+package lab5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EllipseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
